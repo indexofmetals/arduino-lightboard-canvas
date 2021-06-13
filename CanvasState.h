b1bb93fb5cb127c4f8628491e8d2c06ce89543b1
Nick Gudman <nick.gudman@reachtech.com>
2021-06-13 15:38:25 -0700

    adding header file for CanvasState

--- CanvasState.h
+++ CanvasState.h
@@ -1,9 +1,25 @@
 #ifndef CanvasState_H
 #define CanvasState_H
 
-typedef struct CanvasState {
-  unsigned int strokeColor;
-  unsigned int fillColor;
-} CanvasState;
+typedef struct CanvasSetting {
+  uint16 strokeColor;
+  uint16 fillColor;
+} CanvasSetting;
 
+// This is actually a stack of states, but the HTML spec poorly named this class
+class CanvasState {
+  public:
+    CanvasState(unsigned int stackSize);
+    ~CanvaseState();
+
+    void save();      // push
+    void restore();   // pop
+    void peak();
+    void reset();   // Resets to default and clears _stack
+    void getStackSize;
+
+  private:
+    CanvasSetting* _stack;
+    unsigned int _stackSize;
+}
 #endif
